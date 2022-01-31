942619d3fa182afcd091be6877e0446b91b07e8c
adrikikicp el programador <78396568+Adrikikicp@users.noreply.github.com>
2022-01-31 11:07:34 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Helium'
+copyright = '2022, Adrikikicp Studios'
+author = 'Adrikikicp Studios'
 
 release = '0.1'
 version = '0.1.0'
